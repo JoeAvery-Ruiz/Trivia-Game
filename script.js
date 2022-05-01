9392ef763f87acab0d1ad525f6f901fe77610f03
joeavery-ruiz <joeavery96@yahoo.com>
2022-05-01 16:33:55 -0500

    Day 1

---
+++ script.js
@@ -0,0 +1,33 @@
+// start button on main display
+// prompt question
+// timer for 10s
+// show answer after timer ends 
+// random generator for 3 outcomes 
+// score selector for team 
+// score board
+// next question button
+ 
+/* function trivia (question,answer){
+    this.question=question
+    this.answer=answer
+}
+const question1 = new trivia('what is ricky bobbys catchphrase')
+ console.log(question1);
+ const answer1 = new answer("If you ain't first you're last")
+ console.log(answer1);  */
+ 
+ const questionArray = ['quest1','quest2','quest3']
+ const answerArray = ['answer1','answer2','answer3']
+ 
+ const arrayLength = questionArray.length;
+ for (var i = 0; i < arrayLength; i++) {
+     console.log(questionArray[i]);
+     console.log(answerArray[i]);
+   
+ }
+
+ 
+
+
+
+

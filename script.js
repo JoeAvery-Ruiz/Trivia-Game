86934e56328821c3b0d462dd5b9c507d762f457e
joeavery-ruiz <joeavery96@yahoo.com>
2022-05-01 17:04:07 -0500

    punishments

--- script.js
+++ script.js
@@ -18,15 +18,21 @@ const question1 = new trivia('what is ricky bobbys catchphrase')
  
  const questionArray = ['quest1','quest2','quest3']
  const answerArray = ['answer1','answer2','answer3']
+ const drinkArray=['sip','chug','shot']
  
+ function randomRange(myMin, myMax) {
+    return Math.floor(Math.random() * (myMax - myMin + 1) + myMin);
+  }
  const arrayLength = questionArray.length;
  for (var i = 0; i < arrayLength; i++) {
      console.log(questionArray[i]);
      console.log(answerArray[i]);
+     let drink = randomRange(0,2)
+     console.log(drinkArray[drink]);
    
  }
 
- 
+
 
 
 

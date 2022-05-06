759f8bbd2d273f6ee3935fea93770bb1f3f8ce25
joeavery-ruiz <joeavery96@yahoo.com>
2022-05-06 17:07:32 -0500

    update

--- script.js
+++ script.js
@@ -6,6 +6,7 @@
 // score selector for team 
 // score board
 // next question button
+//add name for each team/player
  
 /* function trivia (question,answer){
     this.question=question
@@ -16,6 +17,10 @@ const question1 = new trivia('what is ricky bobbys catchphrase')
  const answer1 = new answer("If you ain't first you're last")
  console.log(answer1);  */
  
+
+
+
+
  const questionArray = ['quest1','quest2','quest3']
  const answerArray = ['answer1','answer2','answer3']
  const drinkArray=['sip','chug','shot']
@@ -32,8 +37,58 @@ const question1 = new trivia('what is ricky bobbys catchphrase')
    
  }
 
+ 
+ timeLeft = 10;
+
+ function countdown() {
+     timeLeft--;
+     document.getElementById("seconds").innerHTML = String( timeLeft );
+     if (timeLeft > 0) {
+         setTimeout(countdown, 1000);
+     }
+ };
+
+ 
+ setTimeout(countdown, 1000);
+
+ let player1 = document.querySelector('.player1-score')
+player1.addEventListener('click', ()=>{
+    
+let score = document.querySelector('.score')
+    
+    score.innerHTML = (parseInt(score.innerHTML) + 10).toString().padStart(4,'0')
+
+    
+    
+    
+    
+    console.log(score)
+    
+
+})
+let player2 = document.querySelector('.player2-score2')
+player2.addEventListener('click', ()=>{
+
+let score2 = document.querySelector('.score2')
+   
+    score2.innerHTML = (parseInt(score2.innerHTML) + 10).toString().padStart(4,'0')
+    console.log(score2)
+ 
+}) 
 
 
 
-
-
+document.getElementById('btn-modal').addEventListener('click', function() {
+    document.getElementById('overlay').classList.add('is-visible');
+    document.getElementById('modal').classList.add('is-visible');
+  });
+  
+  document.getElementById('close-btn').addEventListener('click', function() {
+    document.getElementById('overlay').classList.remove('is-visible');
+    document.getElementById('modal').classList.remove('is-visible');
+  });
+  document.getElementById('overlay').addEventListener('click', function() {
+    document.getElementById('overlay').classList.remove('is-visible');
+    document.getElementById('modal').classList.remove('is-visible');
+  });
+   

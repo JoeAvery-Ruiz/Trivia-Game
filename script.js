7a9f3f007ad59e1dcce2b85419e4887a4effba7a
joeavery-ruiz <joeavery96@yahoo.com>
2022-05-06 22:23:37 -0500

    add more

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// start button on main display
+/* // start button on main display
 // prompt question
 // timer for 10s
 // show answer after timer ends 
@@ -17,7 +17,7 @@ console.log(question1);
 const answer1 = new answer("If you ain't first you're last")
 console.log(answer1);  */
 
-const questionArray = ['quest1','quest2','quest3']
+/*const questionArray = ['quest1','quest2','quest3']
 const answerArray = ['answer1','answer2','answer3']
 const drinkArray=['sip','chug','shot']
 /* let i =0;
@@ -34,7 +34,7 @@ function nextQuestion(){
      //questionArray[i];
  /* }); */
 
- function randomRange(myMin, myMax) {
+ /*function randomRange(myMin, myMax) {
     return Math.floor(Math.random() * (myMax - myMin + 1) + myMin);
   }
   const arrayLength = questionArray.length;
@@ -75,7 +75,7 @@ let score1 = document.querySelector('.score1')
 
  
 
-let player2 = document.querySelector('.player2-score2')
+/*let player2 = document.querySelector('.player2-score2')
 player2.addEventListener('click', ()=>{
 
 let score2 = document.querySelector('.score2')
@@ -98,7 +98,7 @@ let player2score= 0;
 
 let score1 = document.getElementById('score1').innerHTML=player1score;
 let score2 = document.getElementById('score2').innerHTML=player2score; */
-setTimeout (countdown, 1000);
+/*setTimeout (countdown, 1000);
 
 
 let player1 = document.querySelector('.player1-score')
@@ -111,20 +111,74 @@ let score = document.querySelector('.score')
 console.log(score1)
 });
 
+document.getElementById('btn-modal').addEventListener('click', function() {
+    document.getElementById('overlay').classList.add('is-visible');
+    document.getElementById('modal').classList.add('is-visible');
+  });
+  
+  document.getElementById('close-btn').addEventListener('click', function() {
+    document.getElementById('overlay').classList.remove('is-visible');
+    document.getElementById('modal').classList.remove('is-visible');
+  });
+  document.getElementById('overlay').addEventListener('click', function() {
+    document.getElementById('overlay').classList.remove('is-visible');
+    document.getElementById('modal').classList.remove('is-visible');
+  }); */
 
+  const questionArray = ['quest1','quest2','quest3']
+ const answerArray = ['answer1','answer2','answer3']
+ const drinkArray=['sip','chug','shot']
+ 
+ function randomRange(myMin, myMax) {
+    return Math.floor(Math.random() * (myMax - myMin + 1) + myMin);
+  }
+ const arrayLength = questionArray.length;
+ for (var i = 0; i < arrayLength; i++) {
+     console.log(questionArray[i]);
+     console.log(answerArray[i]);
+     let drink = randomRange(0,2)
+     console.log(drinkArray[drink]);
+   
+ }
 
+ 
+ timeLeft = 10;
 
+ function countdown() {
+     timeLeft--;
+     document.getElementById("seconds").innerHTML = String( timeLeft );
+     if (timeLeft > 0) {
+         setTimeout(countdown, 1000);
+     }
+ };
 
+ 
+ setTimeout(countdown, 1000);
 
+ let player1 = document.querySelector('.player1-score1')
+player1.addEventListener('click', ()=>{
+    
+let score1 = document.querySelector('.score1')
+    
+    score1.innerHTML = (parseInt(score1.innerHTML) + 10).toString().padStart(4,'0')
 
+    
+    
+    
+    
+    console.log(score1)
+    
 
+})
+let player2 = document.querySelector('.player2-score2')
+player2.addEventListener('click', ()=>{
 
-
-
-
-
-
-
+let score2 = document.querySelector('.score2')
+   
+    score2.innerHTML = (parseInt(score2.innerHTML) + 10).toString().padStart(4,'0')
+    console.log(score2)
+ 
+}) 
 
 
 
@@ -141,3 +195,4 @@ document.getElementById('btn-modal').addEventListener('click', function() {
     document.getElementById('overlay').classList.remove('is-visible');
     document.getElementById('modal').classList.remove('is-visible');
   });
+   
